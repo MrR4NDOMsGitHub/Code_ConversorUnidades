053d7cb8fa06caa04d329d64011755bd4178ab64
Adrian Aguiar Gonzalez <adrianaguiargonzalez@gmail.com>
2019-05-09 10:24:31 +0200

    Conversion KW a CV

--- src/proyecto/Potencia.java
+++ src/proyecto/Potencia.java
@@ -17,7 +17,8 @@ public class Potencia {
             resp = (String) JOptionPane.showInputDialog(null, "Seleccione una opcion: ", "Menu", JOptionPane.DEFAULT_OPTION, null, opciones, opciones[0]);
             switch (resp) {
                 case "KW a CV":
-
+                    System.out.println("Dime un número en kw");
+                    KWaCV(tec.nextDouble());
                     break;
                 case "CV a KW":
 
@@ -33,5 +34,9 @@ public class Potencia {
                     break;
             }
         } while (!resp.equals("Salir"));
+    }
+    public void KWaCV(double KW){
+        double conversion=KW*1.34102;
+        System.out.println("El número "+KW+" en CV es: "+conversion);
     }
 }
